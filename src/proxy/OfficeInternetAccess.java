b4cf13a55ca978adc4ee1968e5baf1f8e1079efe
HasanNoorJamanSujon <hnjsujon.mbstu@gmail.com>
2018-04-18 23:59:17 +0600

    proxy

---
+++ src/proxy/OfficeInternetAccess.java
@@ -0,0 +1,5 @@
+package src.proxy;
+
+public interface OfficeInternetAccess {
+    void grantInternetAccess();
+}
---
+++ src/proxy/ProxyPatternClient.java
@@ -0,0 +1,9 @@
+package src.proxy;
+
+public class ProxyPatternClient {
+    public static void main(String[] args)
+    {
+        OfficeInternetAccess access = new ProxyInternetAccess("Ashwani Rajput");
+        access.grantInternetAccess();
+    }
+}
