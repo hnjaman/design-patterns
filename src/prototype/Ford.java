9ed9308d2ad94840c29e3c42b37d96062ee787ad
HasanNoorJamanSujon <hnjsujon.mbstu@gmail.com>
2018-04-21 13:07:01 +0600

    prototype vaskaran

---
+++ src/prototype/Ford.java
@@ -0,0 +1,13 @@
+package prototype;
+
+public class Ford extends BasicCar {
+    public Ford(String m)
+    {
+        modelname = m;
+    }
+    @Override
+    public BasicCar clone() throws CloneNotSupportedException
+    {
+        return(Ford)super.clone();
+    }
+}
---
+++ src/prototype/Nano.java
@@ -0,0 +1,13 @@
+package prototype;
+
+public class Nano extends BasicCar {
+    public Nano(String m)
+    {
+        modelname = m;
+    }
+    @Override
+    public BasicCar clone() throws CloneNotSupportedException
+    {
+        return(Nano)super.clone();
+    }
+}
